16edfe2b8bd5b7cab9fbcb4b20c2927019da0ef0
sksthrs <38233367+sksthrs@users.noreply.github.com>
2024-03-21 18:14:28 +0900

    implemented

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # yomiage
+ブラウザだけで簡単に使える音声合成アプリケーションです。
+
+# 使い方
+中央の欄に「喋らせたい言葉」を入れて、下の「読み上げ」ボタンを押すと、中央欄の内容を読み上げます。
+
+上の欄は音声を選ぶのに使えます。Windowsだと複数の声から選べるようです（AndroidやiOSでは1種類しか出ませんでした）。
+
+# 動作確認した環境
+
+- Windows + Chrome
+- Windows + Edge
+- Windows + Firefox
+- Android + Chrome
+- iOS + Safari
+
+たぶんmacでも動くと思います。
+
+# デモ
+https://sksthrs.github.io/yomiage/
---
+++ app.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', (_) => {
+
+  const LANG = "ja"
+  const LANG_safari = "ja-JP"
+
+  /** @type {HTMLButtonElement} */
+  const readAll = document.getElementById('read-all')
+
+  /** @type {HTMLTextAreaElement} */
+  const log = document.getElementById('log')
+
+  const SS = window.speechSynthesis
+
+  /** @type {HTMLSelectElement} */
+  const voiceSelect = document.getElementById('voices')
+
+  /** @type {SpeechSynthesisVoice[]} */
+  let voices = []
+
+  function isSafari() {
+    return window.navigator.userAgent.toLowerCase().indexOf("safari") >= 0
+  }
+
+  function clearVoices() {
+    voices = []
+    for (const opt of voiceSelect.options) {
+      opt.remove()
+    }
+  }
+
+  function populateVoices() {
+    clearVoices()
+
+    if (isSafari()) {
+      voices = SS.getVoices(LANG_safari)
+    } else {
+      voices = SS.getVoices()
+    }
+    console.log(`Enumerate voices... ${voices.length}`)
+    let ix = 0
+    for (const voice of voices) {
+      const source = voice.localService ? 'on-device' : 'internet'
+      console.log(`voices[${ix}] name:[${voice.name}] lang:[${voice.lang}] source:[${source}] default:${voice.default} is-ja:${voice.lang.startsWith(LANG)}`)
+      ix++
+      if (voice.lang.startsWith(LANG) !== true) continue
+
+      console.log(`new voice (${voice.name})...`)
+      const opt = document.createElement('option')
+      opt.textContent = voice.name
+      voiceSelect.appendChild(opt)
+    }
+  }
+
+  /**
+   * Read text by speech synthesis
+   * @param {string} text 
+   * @returns {void}
+   */
+  function readText(text) {
+    console.log(`readText [${text}]`)
+    if (text == null || text.length < 1) return
+
+    const utter = new SpeechSynthesisUtterance(text)
+    utter.lang = LANG_safari
+
+    // Safari seems to inhibit to set these properties
+    if (isSafari() !== true) {
+      utter.pitch = 1
+      utter.rate = 1
+      if (voices.length > 0 && voiceSelect.selectedIndex < voices.length) {
+        utter.voice = voices[voiceSelect.selectedIndex]
+        console.log(`set voice as ${utter.voice.name}`)
+      } else if (voices.length > 0) {
+        utter.voice = voices[0]
+        console.log(`set default-voice as ${utter.voice.name}`)
+      }
+    }
+
+    window.speechSynthesis.speak(utter)
+  }
+
+  // ========== ========== Event handler ========== ==========
+
+  readAll.addEventListener('click', (_) => {
+    const text = log.value
+    readText(text)
+  })
+
+  // for chrome...
+  SS.onvoiceschanged = (_) => {
+    populateVoices()
+  }
+
+  // ========== ========== Procedures ========== ==========
+
+  populateVoices()
+  log.focus()
+  console.log('prepared')
+})
